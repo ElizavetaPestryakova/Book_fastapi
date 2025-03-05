dad03cf60f45c279655d704833d4ddc266d4050c
ElizavetaPestryakova <Lizz1235@yandex.ru>
2025-03-05 18:24:44 +0300

    Delete all data

--- docker/postgres/create_databases.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE book_fastapi_db;
-
-CREATE DATABASE book_fastapi_test_db;
-
-GRANT ALL PRIVILEGES ON DATABASE book_fastapi_db to "postgres_user";
-
-GRANT ALL PRIVILEGES ON DATABASE book_fastapi_test_db to "postgres_user";
--- src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseModel
-from .books import Book
-from .sellers import Seller
-
-__all__ = ['BaseModel', 'Book', 'Seller']
