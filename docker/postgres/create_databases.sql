900539db8a899949268a8b86a9f03566f719d5ea
ElizavetaPestryakova <Lizz1235@yandex.ru>
2025-03-05 15:26:07 +0300

    Очистка ветки main

---
+++ docker/postgres/create_databases.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE book_fastapi_db;
+
+CREATE DATABASE book_fastapi_test_db;
+
+GRANT ALL PRIVILEGES ON DATABASE book_fastapi_db to "postgres_user";
+
+GRANT ALL PRIVILEGES ON DATABASE book_fastapi_test_db to "postgres_user";
